512d180ff8dd6369ab188dfab826977ce678c617
Nitin Raj <nitinraj2001@gmail.com>
2021-06-25 00:04:53 +0530

    set the software required to build this app

---
+++ Online-Quiz-App-Backend/OnlineQuizAppBackend/examserver/src/main/java/com/exam/OnlineQuizAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineQuizAppBackendApplication.class, args);
+	}
+
+}
