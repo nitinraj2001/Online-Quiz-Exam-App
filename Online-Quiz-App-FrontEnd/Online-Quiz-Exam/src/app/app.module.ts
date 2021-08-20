2fb899f6d7ccdbea0bd78167402dfda43c3039e3
Nitin Raj <nitinraj2001@gmail.com>
2021-08-20 09:31:13 +0530

    started user-registration-process-frontend

--- Online-Quiz-App-FrontEnd/Online-Quiz-Exam/src/app/app.module.ts
+++ Online-Quiz-App-FrontEnd/Online-Quiz-Exam/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
